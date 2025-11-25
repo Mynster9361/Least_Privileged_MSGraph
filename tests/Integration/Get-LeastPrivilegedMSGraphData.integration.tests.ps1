01986d5cb61292ffa4f1c28ae0d15b88978ce0c4
-Mynster <66535357+Mynster9361@users.noreply.github.com>
2025-11-25 20:07:05 +0100

    Remove obsolete integration test for Get-LeastPrivilegedMSGraphData

--- tests/Integration/Get-LeastPrivilegedMSGraphData.integration.tests.ps1
+++
@@ -1,9 +0,0 @@
-Describe 'Get-LeastPrivilegedMSGraphData Integration' {
-  It 'Should work in a pipeline' {
-    $names = @('A', 'B')
-    $results = $names | Get-LeastPrivilegedMSGraphData
-    $results.Count | Should -Be 2
-    $results[0].Name | Should -Be 'A'
-    $results[1].Name | Should -Be 'B'
-  }
-}

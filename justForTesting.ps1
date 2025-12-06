ab8ae540fb0239673b9d6c7e161171cd429744de
-Mynster <66535357+Mynster9361@users.noreply.github.com>
2025-12-06 22:00:51 +0100

    fix: justForTesting

--- justForTesting.ps1
+++ justForTesting.ps1
@@ -9,7 +9,8 @@ param(
 
 
 #region temp implementation to load in all functions in the clean folder
-Import-Module .\output\module\LeastPrivilegedMSGraph\0.1.0\LeastPrivilegedMSGraph.psd1
+./build.ps1 -ResolveDependency -tasks clean, build
+Import-Module .\output\module\LeastPrivilegedMSGraph\0.1.2\LeastPrivilegedMSGraph.psd1 -Force -Verbose
 #endregion temp implementation to load in all functions in the clean folder
 
 
@@ -21,15 +22,52 @@ Connect-EntraService -ClientID $clientId -TenantID $tenantId -ClientSecret $clie
 #endregion Initialize log analytics service and connect to msgraph,LogAnalytics with app read all permission
 
 #region the good stuff
-$lightweightGroups = Get-AppRoleAssignment | Select-Object -First 5
+Measure-Command {
+  $lightweightGroups = Get-AppRoleAssignment | select -first 10
 
-$lightweightGroups | Get-AppActivityData -WorkspaceId $logAnalyticsWorkspaceId -Days $daysToQuery
+  $lightweightGroups | Get-AppActivityData -WorkspaceId $logAnalyticsWorkspaceId -Days $daysToQuery -ThrottleLimit 20
 
-$lightweightGroups | Get-AppThrottlingData -WorkspaceId $logAnalyticsWorkspaceId -Days $daysToQuery
+  $lightweightGroups | Get-AppThrottlingData -WorkspaceId $logAnalyticsWorkspaceId -Days $daysToQuery
 
-$lightweightGroups | Get-PermissionAnalysis
+  $lightweightGroups | Get-PermissionAnalysis
 
-Export-PermissionAnalysisReport -AppData $lightweightGroups -OutputPath ".\report.html"
+  Export-PermissionAnalysisReport -AppData $lightweightGroups -OutputPath ".\report.html"
+}
+
+$t = $lightweightGroups | where-object {$_.PrincipalName -eq "OF-Set-IntuneAutopilotDeviceTag-MI"}
+$t | Get-AppActivityData -WorkspaceId $logAnalyticsWorkspaceId -Days $daysToQuery
+$t | Get-AppThrottlingData -WorkspaceId $logAnalyticsWorkspaceId -Days $daysToQuery
+$t | Get-PermissionAnalysis
+
+Get-AppActivityFromLog -logAnalyticsWorkspace $logAnalyticsWorkspaceId -days 30 -spId "aaa067f5-4f3b-4995-9407-8086d0bd39d4" -Debug -Verbose
+<# Before Freds suggestions
+Days              : 0
+Hours             : 0
+Minutes           : 23
+Seconds           : 24
+Milliseconds      : 616
+Ticks             : 14046166944
+TotalDays         : 0,0162571376666667
+TotalHours        : 0,390171304
+TotalMinutes      : 23,41027824
+TotalSeconds      : 1404,6166944
+TotalMilliseconds : 1404616,6944
+
+#>
+
+<# After Freds suggestions
+Days              : 0
+Hours             : 0
+Minutes           : 12
+Seconds           : 56
+Milliseconds      : 76
+Ticks             : 7760765648
+TotalDays         : 0,00898236764814815
+TotalHours        : 0,215576823555556
+TotalMinutes      : 12,9346094133333
+TotalSeconds      : 776,0765648
+TotalMilliseconds : 776076,5648
+#>
 
 #endregion the good stuff
 
